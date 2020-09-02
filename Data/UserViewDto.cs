435cf21f8fb80e5792f97de746de575ef065078c
Codrut Marin <codrutsoft13@gmail.com>
2020-09-02 20:24:07 +0200

    Add user view dto

---
+++ Data/UserViewDto.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Activity.Data {
+    public class UserViewDto {
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string password { private get; set; }
+    }
+}
